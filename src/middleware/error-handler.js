c339009ef7464b89378ae873676713c5b9b5bd8f
Sean Johnson <seanjohnson220@gmail.com>
2021-02-05 08:46:00 -0800

    update to middleware

--- src/middleware/error-handler.js
+++ src/middleware/error-handler.js
@@ -1,7 +1,6 @@
 const { NODE_ENV } = require("../config");
 
 module.exports = function errorHandler(error, req, res, next) {
-	console.log(error)
 	const response =
 		NODE_ENV === "production"
 			? { error: "Server error" }
